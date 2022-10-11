59aad94f4c6afef8644d96d118c4471ed8372d88
unknown <annasantoriello15@gmail.com>
2022-10-11 22:02:16 +0200

    added conditional rendering 01

--- my-app/src/Welcome.js
+++ my-app/src/Welcome.js
@@ -6,8 +6,11 @@ export class Welcome extends React.Component {
         return (
             <div>
                 <p>Welcome, {this.props.name}!</p>
-                <Age age={24} />
+                <Age age= {this.props.age} />
             </div>
         )
     }
 }
+
+//Modify the Welcome component so that the Age component is rendered only if 
+//the age prop is greater than 18.
