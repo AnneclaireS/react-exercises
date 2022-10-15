8f0b68bc8d2638958618688c27a408c93f768717
unknown <annasantoriello15@gmail.com>
2022-10-15 15:43:28 +0200

    added focus

--- my-app/src/UncontrolledLogin.js
+++ my-app/src/UncontrolledLogin.js
@@ -22,7 +22,7 @@ export class UncontrolledLogin extends React.Component {
             <div>
                 <form onSubmit={this.handleFormSubmit}>
                 <h3 style={{color:'purple'}}>Insert your username:</h3>
-                <input name='username' type='text' required></input>
+                <input name='username' type='text' required autoFocus></input>
                 <h3 style={{color:'purple'}}>Insert your password:</h3>
                 <input name='password' type='password' required></input><br/>
                 <label style={{color:'purple'}}><h3>Tick if completed &gt;
