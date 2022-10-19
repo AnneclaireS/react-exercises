5cfd32be1614bec0b925906bb0faac18ebce2923
unknown <annasantoriello15@gmail.com>
2022-10-19 12:04:19 +0200

    added container

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,16 +2,43 @@ import React from "react";
 import { Hello } from './Hello'
 import { Welcome } from "./Welcome"
 import { Counter } from './Counter'
+import { ClickCounter } from "./ClickCounter";
+import { ClickTracker } from './ClickTracker'
+import { InteractiveWelcome } from "./InteractiveWelcome";
+import { Login } from "./Login";
+import { UncontrolledLogin } from "./UncontrolledLogin";
+import { ToDoList } from "./ToDoList";
+import { Container } from "./Container";
+
+const items = [
+
+]
 
 
 export class App extends React.Component {
     render() {
         return (
-            <div>
+            <Container className="container">
                 <Hello />
-                <Welcome name='John' age={23} />
+                <div style={{ borderBottom: '2px solid grey' }}></div>
+                <Welcome name='John' age={20} />
+                <div style={{ borderBottom: '2px solid grey' }}></div>
                 <Counter initialValue={0} incrementAmount={1} incrementInterval={1000} />
-            </div>
+                <div style={{ borderBottom: '2px solid grey' }}></div>
+                <ClickCounter /><br />
+                <div style={{ borderBottom: '2px solid grey' }}></div><br />
+                <ClickTracker />
+                <div style={{ borderBottom: '2px solid grey' }}></div><br />
+                <InteractiveWelcome />
+                <div style={{ borderBottom: '2px solid grey' }}></div>
+                <Login />
+                <div style={{ borderBottom: '2px solid grey' }}></div><br />
+                <UncontrolledLogin />
+                <div style={{ borderBottom: '2px solid grey' }}></div><br />
+                <ToDoList items={items} />
+                <div style={{ borderBottom: '2px solid grey' }}></div><br />
+
+            </Container>
         )
     }
 
---
+++ my-app/src/ClickCounter.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+export class ClickCounter extends React.Component {
+    state = {
+        count: 0
+    }
+
+
+    handleCounterIncrement = () =>{
+
+        this.setState((state) => {
+            return {
+                count: state.count + 1,
+            }
+        })
+    }
+    render(){
+        return(
+            <div>
+            <h2><em>This user's clicked {this.state.count} times!</em></h2>
+            <button onClick={this.handleCounterIncrement}><strong>ClickCounter</strong></button>
+            </div>
+        )
+    }
+}
