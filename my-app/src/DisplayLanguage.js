97b69d9a64d02b74142251aca8b983e35af10230
unknown <annasantoriello15@gmail.com>
2022-10-19 17:18:19 +0200

    added consumer

--- my-app/src/DisplayLanguage.js
+++ my-app/src/DisplayLanguage.js
@@ -1,11 +1,27 @@
 import { LanguageContext }from './LanguageContext';
 
-export class DisplayLanguage extends LanguageContext{
-    render() {
+
+const Selected = {
+    en: {
+        CHANGE: 'You are now on the english website version'
+    },
+    it: {
+        CHANGE: 'Sei ora nella versione italiana del sito'
+    }
+}
+
+export function DisplayLanguage(){
         return(
             <div>
-
+                <LanguageContext.Consumer>
+                    {(language) => {
+                        return (
+                            <div>
+                                <h1>{Selected[language].CHANGE}</h1>
+                            </div>
+                        )
+                    }}
+                </LanguageContext.Consumer>
             </div>
         )
-    }
 }
