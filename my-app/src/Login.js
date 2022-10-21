9cd8d814d6a5c0cd61dd49aa94311c4a301ac079
unknown <annasantoriello15@gmail.com>
2022-10-21 12:14:21 +0200

    added function Login

--- my-app/src/Login.js
+++ my-app/src/Login.js
@@ -1,59 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 
-export class Login extends React.Component {
-    state = {
+export function Login() {
+
+    const [data, setData] = useState({
         username: '',
         password: '',
         remember: false,
         disabled: true
-    }
+    })
 
-    handleLogin = (event) => {
-        
-        const value = event.target.value
-        const name = event.target.name
-        const type = event.target.type
-        const checked = event.target.checked
-        
-        this.setState({
-            [name]: type === 'checkbox' ? checked : value,
-        }) 
-    }
+    const handleInputChange = (event) => {
 
-    onLogin = (event) => {
-        console.log(event)
-        const click = event.target.disabled
-        this.setState({
-            disabled : click
-        })
-        console.log(click)
-    }
+        const { name, value, type, checked } = event.target
 
-    handleResetState = () => {
-        this.setState({
-            username: '',
-            password:'',
-            remember: false
+
+        setData((data) => {
+            return {
+                ...data,
+                [name]: type === 'checkbox' ? checked : value,
+            }
+
         })
     }
 
-    render() {
-        const MyStyle = {
-            backgroundColor: this.state.password.length < 8 ? 'red' : 'green'
-        }
-        return (
-            <div>
-                <h3 style={{color:'blue'}}>Insert your username:</h3>
-                <input name='username' type='text' value={this.state.username} onChange={this.handleLogin}></input>
-                <h3 style={{color:'blue'}}>Insert your password:</h3>
-                <input name='password' type='password' value={this.state.password} onChange={this.handleLogin}></input><br/>
-                <label style={{color:'blue'}}><h3>Tick if completed &gt;
-                <input name='remember' type='checkbox' checked={this.state.remember} onChange={this.handleLogin}></input>
-                </h3></label>
-                <button style={MyStyle} type="submit" disabled={!this.state.username || !this.state.password} onClick={this.onLogin}>Login</button>
-                <button style={{color:'blue', fontSize:14, fontWeight:'bold'}} onClick={this.handleResetState}>Press to reset</button>
-                <br/><br/>
-            </div>
-        )
+    console.log(data)
+
+
+
+
+    const onLogin = (event) => {
+        console.log(data.username, data.password, data.remember)
+        setData((data) => {
+            return {
+                ...data,
+                username: "",
+                password: "",
+                remember: false
+            }
+        })
     }
+
+
+    const handleResetState = () => {
+        
+        setData((data) => {
+            return {
+                ...data,
+                username: "",
+                password: "",
+                remember: false
+            }
+        })
+    }
+
+
+    const MyStyle = {
+        backgroundColor: data.password.length < 8 ? 'red' : 'green'
+    }
+    return (
+        <div>
+            <h3 style={{ color: 'blue' }}>Insert your username:</h3>
+            <input name='username' type='text' value={data.username} onChange={handleInputChange}></input>
+            <h3 style={{ color: 'blue' }}>Insert your password:</h3>
+            <input name='password' type='password' value={data.password} onChange={handleInputChange}></input><br />
+            <label style={{ color: 'blue' }}><h3>Tick if completed &gt;
+                <input name='remember' type='checkbox' checked={data.remember} onChange={handleInputChange}></input>
+            </h3></label>
+            <button type="submit" style={MyStyle} disabled={!data.username || !data.password} onClick={onLogin}>Login</button>
+            <button style={{ color: 'blue', fontSize: 14, fontWeight: 'bold' }} onClick={handleResetState}>Press to reset</button>
+            <br /><br />
+        </div>
+    )
+
 }
