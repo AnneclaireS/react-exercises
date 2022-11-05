4114e126aab15aad1282f5784e67fcb61929f068
unknown <annasantoriello15@gmail.com>
2022-11-05 18:04:28 +0100

    added route counter

--- my-app/src/RootApp.js
+++ my-app/src/RootApp.js
@@ -4,9 +4,9 @@ import { Welcome } from './Welcome';
 export function RootApp() {
     return (
         <Routes >
-            <Route path='/' element={<Welcome  name='Anna'/>}>
-
-            </Route>
+            <Route path='/' element={<Welcome  name='Anna'/>}/>
+            <Route path='counter' element={<Counter  initialValue={0}/>} />
+            
         </Routes>
     )
 }
